1de6b6509d4b88f5be297e6003b24549bb92dab5
listerplus <96763963+listerplus@users.noreply.github.com>
2024-03-03 08:55:44 +1100

    Use By objects for selectors

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -1,40 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using se_csharp_iconnect.Utilities;
 
 namespace se_csharp_iconnect.Pages
 {
     public class LoginPage
     {
-        private readonly IWebDriver _driver;
+        public readonly IWebDriver driver;
 
         public LoginPage(IWebDriver driver)
         {
-            this._driver = driver;
+            this.driver = driver;
         }
 
-        IWebElement TxtUserName => _driver.FindElement(By.Id("UserName"));
-        IWebElement TxtPassword => _driver.FindElement(By.Id("Password"));
-        IWebElement BtnLogin => _driver.FindElement(By.Id("//input[@value='Log in']"));
+        /* Locators
+        IWebElement TxtUserName => driver.FindElement(By.Id("UserName"));
+        IWebElement TxtPassword => driver.FindElement(By.Id("Password"));
+        IWebElement BtnLogin => driver.FindElement(By.XPath("//input[@value='Log in']"));
+        public IWebElement ErrorField => driver.FindElement(By.XPath("//div[@class='validation-summary-errors']/ul/li"));
+        */
 
+        // By Locators
+        public By TxtUserNameBy => By.Id("UserName");
+        public By TxtPasswordBy => By.Id("Password");
+        public By BtnLoginBy => By.XPath("//input[@value='Log in']");
+        public By ErrorFieldBy => By.XPath("//div[@class='validation-summary-errors']/ul/li");
+
+        // Text
         public readonly string userValid = "hari";
         public readonly string passwordValid = "123123";
-        public readonly string userInValid = "hari";
-        public readonly string passwordInValid = "123123";
+        public readonly string userInValid = "test";
+        public readonly string passwordInValid = "test";
+        public readonly string errorInValid = "Invalid username or password.";
 
+        // Methods
         public void ClickLogin() 
         {
-            BtnLogin.Click();
+            WaitUtil.WaitVisible(driver, BtnLoginBy);
+            driver.FindElement(BtnLoginBy).Click();
         }
 
         public void Login(string username, string password) 
         {
-            TxtUserName.SendKeys(userValid);
-            TxtPassword.SendKeys(passwordValid);
-            BtnLogin.Submit();
+            driver.FindElement(TxtUserNameBy).SendKeys(username);
+            driver.FindElement(TxtPasswordBy).SendKeys(password);
+            driver.FindElement(BtnLoginBy).Click();
         }
 
 
