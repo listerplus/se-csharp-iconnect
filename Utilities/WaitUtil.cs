0e5d52d9239c3af7d74f7b4504e22db0d47c7ff7
listerplus <96763963+listerplus@users.noreply.github.com>
2024-03-03 08:56:15 +1100

    Create utility for wait

---
+++ Utilities/WaitUtil.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+
+namespace se_csharp_iconnect.Utilities
+{
+    public class WaitUtil
+    {
+        private const int DefaultTimeout = 10;
+        public static void WaitVisible(IWebDriver driver, By by, int timeoutInSeconds = DefaultTimeout)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            wait.Until(ExpectedConditions.ElementIsVisible(by));
+        }
+
+        public static void WaitClickable(IWebDriver driver, By by, int timeoutInSeconds = DefaultTimeout)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            wait.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+
+        public static void WaitSelected(IWebDriver driver, By by, int timeoutInSeconds = DefaultTimeout)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            wait.Until(ExpectedConditions.ElementToBeSelected(by));
+        }
+
+        // Add more methods based on your needs
+
+        // Example usage:
+        // WaitUtil.WaitVisible(driver, By.Id("exampleId"));
+    }
+}
