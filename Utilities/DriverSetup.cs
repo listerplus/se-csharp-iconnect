6b8efddfbc29fe0dda1a39cf6c88bf9024adaa0f
listerplus <96763963+listerplus@users.noreply.github.com>
2024-03-03 16:49:18 +1100

    Refactor and set uri if null

--- Utilities/DriverSetup.cs
+++ Utilities/DriverSetup.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
@@ -8,31 +9,50 @@ namespace se_csharp_iconnect.Utilities
 {
     public class DriverSetup
     {
-        // https://learn.microsoft.com/en-us/dotnet/api/system.configuration.configurationmanager?view=dotnet-plat-ext-8.0
+        // https://learn.microsoft.com/en-us/dotnet/api/system.configuration.configurationmanager?view=dotnet-plat-ext-8.0 
         // Reads the project's App.config file
-        private static string? browserName = ConfigurationManager.AppSettings["Browser"];
-        public static string? uri = ConfigurationManager.AppSettings["Server"];
+        public static string? browserName = ConfigurationManager.AppSettings["Browser"];
+        private static string? _uri;
+        public static string? Uri
+        {
+            get
+            {
+                _uri = ConfigurationManager.AppSettings["Server"];
+                // If _uri is null or empty, set a hardcode uri 
+                if (string.IsNullOrWhiteSpace(_uri))
+                {
+                    _uri = "horse.industryconnect.io";
+                }
+                return _uri;
+            }
+            set => _uri = value;
+        }
+
         public static IWebDriver BrowserSetup(IWebDriver driver)
         {
-
-            if (browserName.ToLower() == "chrome")
+            string? uri = ConfigurationManager.AppSettings["Server"];
+            if (string.IsNullOrWhiteSpace(browserName))
             {
+                Console.WriteLine("Browser not specified in App.config. Defaulting to Chrome.");
                 driver = new ChromeDriver();
-            }
-            else if (browserName.ToLower() == "firefox")
-            {
-                driver = new FirefoxDriver();
-            }
-            else if (browserName.ToLower() == "edge")
-            {
-                driver = new EdgeDriver();
             }
             else
             {
-                Console.WriteLine("Default Browser Chrome initiated, please check browser details in app.config file");
-                driver = new ChromeDriver();
+                switch (browserName.ToLower())
+                {
+                    case "chrome":
+                        driver = new ChromeDriver();
+                        break;
+                    case "firefox":
+                        driver = new FirefoxDriver();
+                        break;
+                    default:
+                        Console.WriteLine($"Unrecognized browser: {browserName}. Defaulting to Chrome.");
+                        driver = new ChromeDriver();
+                        break;
+                }
             }
-
+            driver.Manage().Window.Maximize();
             return driver;
 
         }
---
+++ Utilities/Util.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace se_csharp_iconnect.Utilities
+{
+    public class Util
+    {
+        public static string GetProjRootDir()
+        {
+            string currentDir = Directory.GetCurrentDirectory();
+            return currentDir.Split("bin")[0];
+        }
+    }
+}
