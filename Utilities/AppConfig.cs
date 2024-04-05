f6ae2369fbf39a786e8620252708592c779fdc1c
listerplus <96763963+listerplus@users.noreply.github.com>
2024-04-05 18:40:58 +1100

    Add configuration for url and browser

---
+++ Utilities/AppConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using RazorEngine.Configuration;
+
+namespace se_csharp_iconnect.Utilities
+{
+    public class AppConfig
+    {
+        public string Browser { get; set; }
+        public string Url { get; set; }
+
+        public static AppConfig LoadConfiguration()
+        {
+            // Needed packages: Microsoft.Extensions.Configuration
+            // Microsoft.Extensions.Configuration.Binder
+            // Microsoft.Extensions.Configuration.Json
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var appConfig = new AppConfig();
+            configuration.GetSection("Settings").Bind(appConfig);
+
+            return appConfig;
+        }
+    }
+}
--- Utilities/DriverSetup.cs
+++ Utilities/DriverSetup.cs
@@ -9,48 +9,23 @@ namespace se_csharp_iconnect.Utilities
 {
     public class DriverSetup
     {
-        // https://learn.microsoft.com/en-us/dotnet/api/system.configuration.configurationmanager?view=dotnet-plat-ext-8.0 
-        // Reads the project's App.config file
-        public static string? browserName = ConfigurationManager.AppSettings["Browser"];
-        private static string? _uri;
-        public static string? Uri
+        public static IWebDriver BrowserSetup(IWebDriver driver, string browserType)
         {
-            get
+            switch (browserType.ToLower())
             {
-                _uri = ConfigurationManager.AppSettings["Server"];
-                // If _uri is null or empty, set a hardcode uri 
-                if (string.IsNullOrWhiteSpace(_uri))
-                {
-                    _uri = "horse.industryconnect.io";
-                }
-                return _uri;
-            }
-            set => _uri = value;
-        }
-
-        public static IWebDriver BrowserSetup(IWebDriver driver)
-        {
-            string? uri = ConfigurationManager.AppSettings["Server"];
-            if (string.IsNullOrWhiteSpace(browserName))
-            {
-                Console.WriteLine("Browser not specified in App.config. Defaulting to Chrome.");
-                driver = new ChromeDriver();
-            }
-            else
-            {
-                switch (browserName.ToLower())
-                {
-                    case "chrome":
-                        driver = new ChromeDriver();
-                        break;
-                    case "firefox":
-                        driver = new FirefoxDriver();
-                        break;
-                    default:
-                        Console.WriteLine($"Unrecognized browser: {browserName}. Defaulting to Chrome.");
-                        driver = new ChromeDriver();
-                        break;
-                }
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    Console.WriteLine($"Unrecognized browser: {browserType}. Defaulting to Chrome.");
+                    driver = new ChromeDriver();
+                    break;
             }
             driver.Manage().Window.Maximize();
             return driver;
